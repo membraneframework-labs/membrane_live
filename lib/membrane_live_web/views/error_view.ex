93ad576a0ed6fc9c3d0645632804bfa8906cb2f5
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2022-07-15 12:29:17 +0200

    Dialyzer finally happy

--- lib/membrane_live_web/views/error_view.ex
+++ lib/membrane_live_web/views/error_view.ex
@@ -1,7 +1,7 @@
 defmodule MembraneLiveWeb.ErrorView do
   use MembraneLiveWeb, :view
 
-  @spec template_not_found(any, any) :: any
+  # credo:disable-for-next-line
   def template_not_found(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
   end
