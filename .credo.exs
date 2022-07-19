f1c618e8a0a6a6fef5c4e572bb9b19862166f981
Łukasz Wala <lukasz.wala@swmansion.com>
2022-07-19 12:50:03 +0200

    Actually add circleCI config, see: previous commit

--- .credo.exs
+++ .credo.exs
@@ -1,3 +1,9 @@
+# This file contains the configuration for Credo and you are probably reading
+# this after creating it with `mix credo.gen.config`.
+#
+# If you find anything wrong or unclear in this file, please report an
+# issue on GitHub: https://github.com/rrrene/credo/issues
+#
 %{
   configs: [
     %{
