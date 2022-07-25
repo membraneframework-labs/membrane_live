2650d340f163ed7a7230df7e797c43355ac144d5
Łukasz Wala <lukasz.wala@swmansion.com>
2022-07-25 11:44:11 +0200

    Stop the popup from showing up on start

--- assets/js/pages/Event.tsx
+++ assets/js/pages/Event.tsx
@@ -30,7 +30,7 @@ const Event = () => {
   const csrfToken = document.querySelector("meta[name='csrf-token']")?.getAttribute("content");
   const navigate = useNavigate();
   const [eventInfo, setEventInfo] = useState<EventInfo>(initEventInfo());
-  const [isPresenterPopupOpen, setIsPresenterPopupOpen] = useState<boolean>(true); // will be triggered with call from server
+  const [isPresenterPopupOpen, setIsPresenterPopupOpen] = useState<boolean>(false); // will be triggered with call from server
   const [participants, setParticipants] = useState<string[]>([]);
   const socket = new Socket("/socket");
   socket.connect();
