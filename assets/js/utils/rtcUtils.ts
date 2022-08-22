c959129793d81f4bda7f4e8d6c31690a54c022cc
Karol Konkol <56369082+Karolk99@users.noreply.github.com>
2022-08-22 09:18:32 +0200

    Create screen share view marged with camera view (#40)
    * Create screen share view marged with camera view
    
    * Fix changing from screen share to screen share

--- assets/js/utils/rtcUtils.ts
+++ assets/js/utils/rtcUtils.ts
@@ -1,35 +1,30 @@
 import { MembraneWebRTC, SerializedMediaEvent } from "@membraneframework/membrane-webrtc-js";
+import { AUDIO_CONSTRAINTS, VIDEO_CONSTRAINTS } from "./const";
+import { getMergedTracks } from "./canvasUtils";
 
 export type Sources = {
   audio: MediaDeviceInfo[];
   video: MediaDeviceInfo[];
 };
-
 export type SourceType = "audio" | "video";
 
+export type MergedScreenRef = {
+  screenTrack: MediaStreamTrack | undefined;
+  cameraTrack: MediaStreamTrack | undefined;
+  refreshId: number | undefined;
+};
+
 export const presenterStreams: { [key: string]: MediaStream } = {};
+export const mergedScreenRef: MergedScreenRef = {
+  screenTrack: undefined,
+  cameraTrack: undefined,
+  refreshId: undefined,
+};
+
+const sourceIds: { audio: string; video: string } = { audio: "", video: "" };
 
 export const findTrackByType = (name: string, sourceType: SourceType) => {
   return presenterStreams[name].getTracks().find((elem) => elem.kind == sourceType);
-};
-
-const addOrReplaceTrack = (
-  name: string,
-  track: MediaStreamTrack,
-  playerCallback: (sourceType: SourceType) => void
-) => {
-  if (!presenterStreams[name]) presenterStreams[name] = new MediaStream();
-  const curTrack = findTrackByType(name, track.kind as SourceType);
-  if (curTrack) {
-    curTrack.stop();
-    presenterStreams[name].removeTrack(curTrack);
-  }
-  presenterStreams[name].addTrack(track);
-  playerCallback(track.kind as SourceType); // to attach MediaStream to HTMLVideoElement object in DOM
-};
-
-const removeStream = (name: string) => {
-  delete presenterStreams[name];
 };
 
 export const changeTrackIsEnabled = (name: string, sourceType: SourceType) => {
@@ -39,27 +34,6 @@ export const changeTrackIsEnabled = (name: string, sourceType: SourceType) => {
 
 export const getCurrentDeviceName = (clientName: string, sourceType: SourceType) => {
   return findTrackByType(clientName, sourceType)?.label;
-};
-
-const AUDIO_CONSTRAINTS: MediaStreamConstraints = {
-  audio: true,
-  video: false,
-};
-
-const VIDEO_CONSTRAINTS: MediaStreamConstraints = {
-  audio: false,
-  video: { width: 1280, height: 720, frameRate: 24 },
-};
-
-const getConstraint = (constraint: MediaStreamConstraints, deviceId: string) => {
-  const newConstraint: MediaStreamConstraints = { audio: false, video: false };
-  const type: SourceType = !constraint.audio ? "video" : "audio";
-  newConstraint[type] = { ...(constraint[type] as Object), deviceId: { exact: deviceId } };
-  return newConstraint;
-};
-
-const filterDevices = (allDevices: MediaDeviceInfo[], type: String) => {
-  return allDevices.filter((device) => device.deviceId != "default" && device.kind == type);
 };
 
 export const getSources = async () => {
@@ -94,8 +68,6 @@ export const setSourceById = async (
     console.error("Couldn't get microphone permission:", error);
   }
 };
-
-const sourceIds: { audio: string; video: string } = { audio: "", video: "" };
 
 export const connectWebrtc = async (
   webrtcChannel: any,
@@ -171,6 +143,7 @@ export const changeSource = async (
   sourceType: SourceType,
   playerCallback: (sourceType: SourceType) => void
 ) => {
+  if (mergedScreenRef.refreshId && sourceType == "video") removeMergedStream();
   await setSourceById(clientName, deviceId, sourceType, playerCallback);
   const newTrack = findTrackByType(clientName, sourceType);
   if (!webrtc || !newTrack) return;
@@ -183,4 +156,62 @@ export const leaveWebrtc = (webrtc: MembraneWebRTC, clientName: string, webrtcCh
   presenterStreams[clientName].getTracks().forEach((track) => track.stop());
   removeStream(clientName);
   webrtc.leave();
+};
+
+export const shareScreen = async (
+  webrtc: MembraneWebRTC,
+  clientName: string,
+  playerCallback: (SourceType: SourceType) => void
+) => {
+  const mergedStream = await getMergedTracks(mergedScreenRef, presenterStreams[clientName]);
+
+  mergedStream.getTracks().forEach((track) => {
+    addOrReplaceTrack(clientName, track, playerCallback);
+  });
+
+  const newTrack = findTrackByType(clientName, "video");
+
+  if (!webrtc || !newTrack) return;
+  if (sourceIds["video"]) webrtc.replaceTrack(sourceIds["video"], newTrack);
+  else sourceIds["video"] = webrtc.addTrack(newTrack, presenterStreams[clientName]);
+};
+
+const getConstraint = (constraint: MediaStreamConstraints, deviceId: string) => {
+  const newConstraint: MediaStreamConstraints = { audio: false, video: false };
+  const type: SourceType = !constraint.audio ? "video" : "audio";
+  newConstraint[type] = { ...(constraint[type] as Object), deviceId: { exact: deviceId } };
+  return newConstraint;
+};
+
+const filterDevices = (allDevices: MediaDeviceInfo[], type: String) => {
+  return allDevices.filter((device) => device.deviceId != "default" && device.kind == type);
+};
+
+const addOrReplaceTrack = (
+  name: string,
+  track: MediaStreamTrack,
+  playerCallback: (sourceType: SourceType) => void
+) => {
+  if (!presenterStreams[name]) presenterStreams[name] = new MediaStream();
+  const curTrack = findTrackByType(name, track.kind as SourceType);
+  if (curTrack) {
+    curTrack.stop();
+    presenterStreams[name].removeTrack(curTrack);
+  }
+  presenterStreams[name].addTrack(track);
+  playerCallback(track.kind as SourceType); // to attach MediaStream to HTMLVideoElement object in DOM
+};
+
+const removeMergedStream = () => {
+  mergedScreenRef.screenTrack?.stop();
+  mergedScreenRef.cameraTrack?.stop();
+  mergedScreenRef.screenTrack = undefined;
+  mergedScreenRef.cameraTrack = undefined;
+
+  clearTimeout(mergedScreenRef.refreshId);
+  mergedScreenRef.refreshId = undefined;
+};
+
+const removeStream = (name: string) => {
+  delete presenterStreams[name];
 };
