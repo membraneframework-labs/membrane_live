772748e10b2005e5661d192aca8fe333add55a3b
Łukasz Wala <lukasz.wala@swmansion.com>
2022-08-24 15:10:43 +0200

    Refector migrations

--- priv/repo/migrations/20220718102030_create_webinars.exs
+++ priv/repo/migrations/20220718102030_create_webinars.exs
@@ -2,13 +2,12 @@ defmodule MembraneLive.Repo.Migrations.CreateWebinars do
   use Ecto.Migration
 
   def change do
-    create table(:webinars) do
-      add :title, :string
-      add :start_date, :naive_datetime
-      add :description, :string
-      add :presenters, {:array, :string}
-      add :viewer_link, :string
-      add :moderator_link, :string
+    create table(:webinars, primary_key: false) do
+      add(:uuid, :binary_id, primary_key: true)
+      add(:title, :string)
+      add(:start_date, :naive_datetime)
+      add(:description, :string)
+      add(:presenters, {:array, :string})
 
       timestamps()
     end
