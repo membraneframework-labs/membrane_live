0e1c05d8ee72515a96530c11571126f5f7450aa6
KarolK99 <konkolisz@gmail.com>
2022-10-25 15:31:29 +0200

    Add new player:

--- assets/js/components/event/HlsPlayer.tsx
+++ assets/js/components/event/HlsPlayer.tsx
@@ -1,9 +1,32 @@
-import React from "react";
-import ReactHlsPlayer from "react-hls-player";
-import { RotateLeft } from "react-swm-icon-pack";
+import React, { useEffect } from "react";
+import {
+  MediaController,
+  MediaControlBar,
+  MediaTimeRange,
+  MediaTimeDisplay,
+  MediaVolumeRange,
+  MediaPlayButton,
+  MediaSeekBackwardButton,
+  MediaSeekForwardButton,
+  MediaMuteButton,
+  MediaFullscreenButton,
+} from "media-chrome/dist/react";
+import "hls-video-element";
+import {
+  Speaker2,
+  Speaker1,
+  Speaker0,
+  SpeakerCross,
+  Play,
+  Pause,
+  RotateLeft,
+  RotateRight,
+  Fullscreen,
+} from "react-swm-icon-pack";
 import "../../../css/event/hlsplayer.css";
 import AnimationComponent from "./HeartAnimation";
 import { Channel } from "phoenix";
+import ReactHlsPlayer from "react-hls-player";
 
 type HlsPlayerProps = {
   hlsUrl: string;
@@ -16,18 +39,60 @@ const HlsPlayer = ({ hlsUrl, presenterName, eventChannel }: HlsPlayerProps) => {
     <div className="HlsStream">
       {hlsUrl ? (
         <div className="HlsPlayerWrapper">
-          <ReactHlsPlayer
-            src={hlsUrl}
-            hlsConfig={{
-              liveSyncDurationCount: 2,
-              initialLifeManifestSize: 2,
-              backBufferLength: 30,
-            }}
-            autoPlay={true}
-            muted={true}
-            controls
-            className="HlsPlayer"
-          />
+          <MediaController className="HlsPlayerWrapper">
+            <ReactHlsPlayer
+              hlsConfig={{liveSyncDuration: 10 }}
+              className="HlsPlayer"
+              autoPlay={true}
+              slot="media"
+              src={hlsUrl}
+              muted={true}
+            ></ReactHlsPlayer>
+            <MediaControlBar className="MediaControlBar">
+              <div className="TopBar">
+                <MediaTimeRange className="MediaTimeRange"></MediaTimeRange>
+                <MediaTimeDisplay className="MediaTimeDisplay" showDuration></MediaTimeDisplay>
+              </div>
+              <div className="BottomBar">
+                <div className="BottomBarLeft">
+                  <MediaMuteButton className="MediaMuteButton">
+                    <SpeakerCross color="white" slot="off"></SpeakerCross>
+                    <Speaker0 color="white" slot="low"></Speaker0>
+                    <Speaker1 color="white" slot="medium"></Speaker1>
+                    <Speaker2 color="white" slot="high"></Speaker2>
+                  </MediaMuteButton>
+                  <MediaVolumeRange className="MediaVolumeRange"></MediaVolumeRange>
+                </div>
+
+                <div className="BottomBarCenter">
+                  <MediaSeekBackwardButton className="MediaSeekBackwardButton" seekOffset={15}>
+                    <RotateLeft className="RewindButtons" slot="backward"></RotateLeft>
+                  </MediaSeekBackwardButton>
+                  <MediaPlayButton pause={Pause} play={Play} className="MediaPlayButton">
+                    <Pause color="white" slot="pause"></Pause>
+                    <Play color="white" slot="play"></Play>
+                  </MediaPlayButton>
+                  <MediaSeekForwardButton className="MediaSeekForwardButton" seekOffset={15}>
+                    <RotateRight
+                      className="RewindButtons"
+                      color="white"
+                      slot="forward"
+                    ></RotateRight>
+                  </MediaSeekForwardButton>
+                </div>
+                <div className="BottomBarRight">
+                  <MediaFullscreenButton className="MediaFullScreenButton">
+                    <Fullscreen
+                      className="FullScreenButton"
+                      color="white"
+                      slot="enter"
+                    ></Fullscreen>
+                    <Fullscreen className="FullScreenButton" color="white" slot="exit"></Fullscreen>
+                  </MediaFullscreenButton>
+                </div>
+              </div>
+            </MediaControlBar>
+          </MediaController>
           <div className="HlsTopBar">
             {presenterName && <div className="HlsPresenterName">{presenterName}</div>}
           </div>
