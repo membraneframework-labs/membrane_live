26894eb529fb7c675eb15f0ad43a7ed4a6b5df08
KarolK99 <konkolisz@gmail.com>
2022-10-28 11:00:18 +0200

    Move hls control bar to separate component

--- assets/js/components/event/HlsPlayer.tsx
+++ assets/js/components/event/HlsPlayer.tsx
@@ -1,31 +1,10 @@
 import React from "react";
 import AnimationComponent from "./HeartAnimation";
 import ReactHlsPlayer from "react-hls-player";
+import HlsControlBar from "./HlsControlBar";
 import { Channel } from "phoenix";
-import {
-  MediaController,
-  MediaControlBar,
-  MediaTimeRange,
-  MediaTimeDisplay,
-  MediaVolumeRange,
-  MediaPlayButton,
-  MediaSeekBackwardButton,
-  MediaSeekForwardButton,
-  MediaMuteButton,
-  MediaFullscreenButton,
-  MediaPipButton,
-} from "media-chrome/dist/react";
-import {
-  Speaker2,
-  Speaker1,
-  Speaker0,
-  SpeakerCross,
-  Play,
-  Pause,
-  RotateLeft,
-  RotateRight,
-  Fullscreen,
-} from "react-swm-icon-pack";
+import { MediaController } from "media-chrome/dist/react";
+import { RotateLeft } from "react-swm-icon-pack";
 import "../../../css/event/hlsplayer.css";
 
 type HlsPlayerProps = {
@@ -52,50 +31,7 @@ const HlsPlayer = ({ hlsUrl, presenterName, eventChannel }: HlsPlayerProps) => {
               slot="media"
               src={hlsUrl}
             ></ReactHlsPlayer>
-            <MediaControlBar className="MediaControlBar">
-              <div className="TopBar">
-                <MediaTimeRange className="MediaTimeRange MediaBackground"></MediaTimeRange>
-                <MediaTimeDisplay
-                  className="MediaTimeDisplay MediaBackground"
-                  showDuration
-                ></MediaTimeDisplay>
-              </div>
-              <div className="BottomBar">
-                <div className="BottomBarLeft">
-                  <MediaMuteButton className="MediaMuteButton MediaBackground">
-                    <SpeakerCross className="ControlButtons" slot="off"></SpeakerCross>
-                    <Speaker0 className="ControlButtons" slot="low"></Speaker0>
-                    <Speaker1 className="ControlButtons" slot="medium"></Speaker1>
-                    <Speaker2 className="ControlButtons" slot="high"></Speaker2>
-                  </MediaMuteButton>
-                  <MediaVolumeRange className="MediaVolumeRange MediaBackground"></MediaVolumeRange>
-                </div>
-
-                <div className="BottomBarCenter">
-                  <MediaSeekBackwardButton className="MediaBackground" seekOffset={15}>
-                    <RotateLeft className="ControlButtons" slot="backward"></RotateLeft>
-                  </MediaSeekBackwardButton>
-                  <MediaPlayButton
-                    pause={Pause}
-                    play={Play}
-                    className="MediaPlayButton MediaBackground"
-                  >
-                    <Pause className="ControlButtons" slot="pause"></Pause>
-                    <Play className="ControlButtons" slot="play"></Play>
-                  </MediaPlayButton>
-                  <MediaSeekForwardButton className="MediaBackground" seekOffset={15}>
-                    <RotateRight className="ControlButtons" slot="forward"></RotateRight>
-                  </MediaSeekForwardButton>
-                </div>
-                <div className="BottomBarRight">
-                  <MediaPipButton className="MediaBackground"></MediaPipButton>
-                  <MediaFullscreenButton className="MediaBackground">
-                    <Fullscreen className="ControlButtons" slot="enter"></Fullscreen>
-                    <Fullscreen className="ControlButtons" slot="exit"></Fullscreen>
-                  </MediaFullscreenButton>
-                </div>
-              </div>
-            </MediaControlBar>
+            <HlsControlBar></HlsControlBar>
           </MediaController>
           <div className="HlsTopBar">
             {presenterName && <div className="HlsPresenterName">{presenterName}</div>}
