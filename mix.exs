cf3fb858ec85b038aebe1905b5c672e5800cf8fc
blazpie <blazej.pierzak@gmail.com>
2023-05-29 13:25:08 +0200

    Feature/hls delta manifest (#182)
    * add handling requests for delta playlists
    
    * lint
    
    * remove asdf files
    
    * update gitignore
    
    * set rtc_engine ver to master
    
    * add commit ref

--- mix.exs
+++ mix.exs
@@ -64,8 +64,7 @@ defmodule Membrane.Live.Mixfile do
       {:esbuild, "~> 0.6", runtime: Mix.env() == :dev},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false},
-      {:membrane_rtc_engine,
-       github: "jellyfish-dev/membrane_rtc_engine", branch: "handle-screen-sharing-hls"},
+      {:membrane_rtc_engine, github: "jellyfish-dev/membrane_rtc_engine", ref: "640cb15"},
 
       # Otel
       {:opentelemetry, "1.0.5"},
@@ -73,7 +72,7 @@ defmodule Membrane.Live.Mixfile do
       {:opentelemetry_zipkin, "1.0.0"},
       {:opentelemetry_exporter, "1.0.4"},
       {:membrane_h264_plugin, "~> 0.2.0"},
-      {:membrane_mp4_plugin, "~> 0.19.0"},
+      {:membrane_mp4_plugin, "~> 0.21.0"},
       {:membrane_aac_plugin, "~> 0.13.0"},
       {:membrane_opus_plugin, "~> 0.16.0"},
       {:membrane_aac_fdk_plugin, "~> 0.14.0"},
@@ -85,7 +84,7 @@ defmodule Membrane.Live.Mixfile do
       {:membrane_ffmpeg_swscale_plugin, "~> 0.11.1"},
       {:membrane_video_compositor_plugin, "~> 0.3.1"},
       {:membrane_framerate_converter_plugin, "~> 0.6.0"},
-      {:membrane_http_adaptive_stream_plugin, "~> 0.13.0"}
+      {:membrane_http_adaptive_stream_plugin, "~> 0.14.0"}
     ]
   end
 
